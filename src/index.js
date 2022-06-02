3188974bf69f2b0debf78650abd794bb875a3573
Hippolyte EZIAN <polarusdev@gmail.com>
2022-06-02 15:00:49 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Bonjour from "./bonjour";
-import Welcome from "./welcome";
 
 const root = ReactDOM.createRoot(document.getElementById("container"));
-root.render(
-  <React.StrictMode>
-    <Bonjour name="Omar" prenom="liqsudklqsjdkqs" />
-    <Bonjour name="Hajar" />
-    <Bonjour name="Salah" />
-    <Bonjour name="Zakaria" />
-    {/* <Welcome /> */}
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
